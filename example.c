728484bbcb0e7eef6ba3ee401caec9a7ad867c52
longwatermelon <longwatermelon1@gmail.com>
2022-07-09 11:21:42 -0700

    window

--- example.c
+++ example.c
@@ -1,9 +1,45 @@
 #include "graph.h"
 #include <stdio.h>
+#include <stdbool.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 
 
 int main(int argc, char **argv)
 {
-    print();
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
+
+    SDL_Window *w = SDL_CreateWindow("Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 800, SDL_WINDOW_SHOWN);
+    SDL_Renderer *r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    bool running = true;
+    SDL_Event evt;
+
+    while (running)
+    {
+        while (SDL_PollEvent(&evt))
+        {
+            switch (evt.type)
+            {
+            case SDL_QUIT:
+                running = false;
+                break;
+            }
+        }
+
+        SDL_RenderClear(r);
+
+        SDL_SetRenderDrawColor(r, 0, 0, 0, 255);
+        SDL_RenderPresent(r);
+    }
+
+    SDL_DestroyRenderer(r);
+    SDL_DestroyWindow(w);
+
+    IMG_Quit();
+    SDL_Quit();
+
+    return 0;
 }
 
