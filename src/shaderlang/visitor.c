c14940d93d939697c9374c78fd5662624596279e
longwatermelon <longwatermelon1@gmail.com>
2022-07-12 18:43:09 -0700

    fix operators

--- src/shaderlang/visitor.c
+++ src/shaderlang/visitor.c
@@ -124,12 +124,8 @@ struct Node *visitor_visit_param(struct Node *n)
 }
 
 #define BINOP_EXECUTE(a, b, op, res) { \
-    switch (op) \
-    { \
-    case BINOP_ADD: res = a + b; break; \
-    case BINOP_SUB: res = a - b; break; \
-    default: break; \
-    } \
+    if (op == BINOP_ADD) res = a + b; \
+    if (op == BINOP_SUB) res = a - b; \
 }
 
 struct Node *visitor_visit_binop(struct Node *n)
@@ -149,10 +145,10 @@ struct Node *visitor_visit_binop(struct Node *n)
     switch (l->type)
     {
     case NODE_FLOAT:
-        BINOP_EXECUTE(l->float_value, r->float_value, l->type, n->op_res->float_value);
+        BINOP_EXECUTE(l->float_value, r->float_value, n->op, n->op_res->float_value);
         break;
     case NODE_INT:
-        BINOP_EXECUTE(l->int_value, r->int_value, l->type, n->op_res->int_value);
+        BINOP_EXECUTE(l->int_value, r->int_value, n->op, n->op_res->int_value);
         break;
     default:
         fprintf(stderr, "[visitor_visit_binop] Error: %d is not a data type.\n",
